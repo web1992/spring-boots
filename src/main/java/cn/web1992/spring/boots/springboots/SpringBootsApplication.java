144654b8ef06e4f28953cf93f79b5c25c3cf48fc
web1992 <554933654@qq.com>
2020-02-22 23:38:05 +0800

    first commit

---
+++ src/main/java/cn/web1992/spring/boots/springboots/SpringBootsApplication.java
@@ -0,0 +1,13 @@
+package cn.web1992.spring.boots.springboots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/web1992/spring/boots/springboots/SpringBootsApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.web1992.spring.boots.springboots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
