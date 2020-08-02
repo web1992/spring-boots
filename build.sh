07b87b57d8c2194ab367173df4bc163511ee4606
web1992 <554933654@qq.com>
2020-08-02 23:18:17 +0800

    add build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,11 @@
 
 mvn clean package -Dmaven.test.skip=true
 
-docker build -t registry.cn-hangzhou.aliyuncs.com/web1992/spring-boots:v2 .
+#imagesid=`docker images |awk -F" " 'NR==2 {print $3}'`
+imagesid=`date +%s`
 
-docker push registry.cn-hangzhou.aliyuncs.com/web1992/spring-boots:v3
+echo ${imagesid}
+
+docker build -t registry.cn-hangzhou.aliyuncs.com/web1992/spring-boots:${imagesid} .
+
+docker push registry.cn-hangzhou.aliyuncs.com/web1992/spring-boots:${imagesid}
